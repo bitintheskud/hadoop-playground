3daec26b8025a1f2c8474c69f986a1923bfc9064
root <root@ns318934.ip-188-165-225.eu>
2017-10-16 22:54:23 +0200

    enhance firewall.sh

--- utils/firewalld.sh
+++ utils/firewalld.sh
@@ -3,14 +3,22 @@
 # Desc : if you working on a public network you might want to
 #        filter only with your network/ip_address. 
 #
+
   
 # enable firewalld 
 yum install -y firewalld
 sudo systemctl start firewalld.service
 
 # filter traffic
-#sudo firewall-cmd --zone=public --permanent --add-source=YOUR_IP/32
+# ok let's live dangerously... 
+MY_IP="$(echo $SSH_CLIENT|cut -d' ' -f1)"
+sudo firewall-cmd --zone=public --permanent --add-source=${MY_IP}/32 || exit 1
 # or 
 #sudo firewall-cmd --zone=public --permanent --add-source=YOUR_NETWORK/MASK  
+
+
 # hadoop port
-#sudo firewall-cmd --zone=public --permanent --add-port=8020,8088,8030,8031,8032,8033
+sudo firewall-cmd --zone=public --permanent --add-port=8030-8033/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=8088/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=8020/tcp 
+sudo firewall-cmd --reload
